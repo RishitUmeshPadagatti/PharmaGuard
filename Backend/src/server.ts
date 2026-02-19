af51e29bbe296ac440ccdb67f62aee4eccd69b95
Ritabrata Ghosh <76sonali40@gmail.com>
2026-02-20 00:14:15 +0530

    backend:02

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,10 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
 import analyzeRoute from "./routes/analyze.route.js";
+import session from "express-session";
 
 dotenv.config();
 
 const app = express();
+app.use(
+  session({
+    secret: "pharmaguard-secret",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 1000 * 60 * 30, // 30 minutes
+    },
+  }),
+);
 app.use(express.json());
 
 app.use("/api/analyze", analyzeRoute);
