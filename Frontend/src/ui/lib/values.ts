18c19525e9ad40596766fbea894fa778f24b96ac
Rishit Umesh Padagatti <rishitpadagatti@gmail.com>
2026-02-20 09:02:19 +0530

    changed ngrok

--- Frontend/src/ui/lib/values.ts
+++ Frontend/src/ui/lib/values.ts
@@ -1,2 +1,3 @@
 // export const BACKEND_URL = "http://localhost:8000"
-export const BACKEND_URL = "https://untaciturnly-inappreciable-casey.ngrok-free.dev"
+// export const BACKEND_URL = "https://untaciturnly-inappreciable-casey.ngrok-free.dev" // rita's
+export const BACKEND_URL = "https://manda-packable-unscandalously.ngrok-free.dev" // rishit's
